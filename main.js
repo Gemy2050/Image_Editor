3326a9cb4e1006e368b06bd018e6460e8b43769b
Mohamed Omar <gemy46349@gmail.com>
2022-07-17 18:58:50 +0200

    Modified

---
+++ main.js
@@ -0,0 +1,89 @@
+let saturate = document.getElementById("saturate");
+let contrast = document.getElementById("contrast");
+let brightness = document.getElementById("brightness");
+let sepia = document.getElementById("sepia");
+let grayscale = document.getElementById("grayscale");
+let blur = document.getElementById("blur");
+let hueRotate = document.getElementById("hue-rotate");
+let upload = document.getElementById("upload");
+let download = document.getElementById("download");
+let img = document.getElementById("img");
+let reset = document.querySelector("span");
+let imgBox = document.querySelector(".img-box");
+let uploadLabel = document.querySelector(".upload label");
+let canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+
+function resetValues() {
+    img.style.filter = "none";
+    saturate.value = '100';
+    contrast.value = '100';
+    brightness.value = '100';
+    sepia.value = '0';
+    grayscale.value = '0';
+    blur.value = '0';
+    hueRotate.value = '0';
+
+}
+
+
+window.onload = function() {
+    download.style.display = "none";
+    reset.style.display = "none";
+    imgBox.style.display = "none";
+}
+upload.onchange = function() {
+    resetValues();
+    download.style.display = "block";
+    reset.style.display = "block";
+    imgBox.style.display = "block";
+    let file = new FileReader();
+    file.readAsDataURL(upload.files[0]);
+    file.onload = () => {
+    img.src = file.result;
+    uploadLabel.textContent = "Change";
+    }
+
+    img.onload = function() {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        img.style.display = "none";
+    }
+}
+
+let filters = document.querySelectorAll("ul li input");
+filters.forEach((filter) => {
+    filter.addEventListener("input", function() {
+        ctx.filter = `
+            saturate(${saturate.value}%)
+            contrast(${contrast.value}%)
+            brightness(${brightness.value}%)
+            sepia(${sepia.value}%)
+            grayscale(${grayscale.value})
+            blur(${blur.value}px)
+            hue-rotate(${hueRotate.value}deg)
+        `
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    });
+});
+
+reset.onclick = function() {
+    resetValues();
+    canvas.style.display= "none";
+    img.style.display = "block"; 
+
+    filters.forEach((f) => {
+        f.onchange = function() {
+            img.style.display = "none";
+            canvas.style.display= "block";
+        }
+    });
+};
+
+
+download.onclick = function() {
+    var dt = canvas.toDataURL('image/jpeg');
+    this.href = dt;
+}
